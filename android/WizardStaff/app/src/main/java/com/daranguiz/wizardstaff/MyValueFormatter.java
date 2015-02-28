570da4c7f5f20fe8eff4a28aa27010df283d3ebf
Dario <aranguizdario@gmail.com>
2015-02-28 00:12:07 -0600

    basic chart init done

---
+++ android/WizardStaff/app/src/main/java/com/daranguiz/wizardstaff/MyValueFormatter.java
@@ -0,0 +1,20 @@
+package com.daranguiz.wizardstaff;
+
+import com.github.mikephil.charting.utils.ValueFormatter;
+
+import java.text.DecimalFormat;
+
+public class MyValueFormatter implements ValueFormatter {
+
+    private DecimalFormat mFormat;
+
+    public MyValueFormatter() {
+        mFormat = new DecimalFormat("###,###,###,##0.0");
+    }
+
+    @Override
+    public String getFormattedValue(float value) {
+        return mFormat.format(value) + " $";
+    }
+
+}
--- android/WizardStaff/app/src/main/java/com/daranguiz/wizardstaff/ScoreboardActivity.java
+++ android/WizardStaff/app/src/main/java/com/daranguiz/wizardstaff/ScoreboardActivity.java
@@ -1,17 +1,77 @@
 package com.daranguiz.wizardstaff;
 
+import android.graphics.PointF;
+import android.graphics.RectF;
+import android.graphics.Typeface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.Highlight;
+import com.github.mikephil.charting.utils.ValueFormatter;
 
-public class ScoreboardActivity extends ActionBarActivity {
+
+public class ScoreboardActivity extends ActionBarActivity implements
+        OnChartValueSelectedListener {
+
+    protected BarChart mChart;
+    private Typeface mTf;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scoreboard);
+
+        mChart = (BarChart) findViewById(R.id.chart1);
+        mChart.setOnChartValueSelectedListener(this);
+
+        mChart.setDrawValueAboveBar(true);
+        mChart.setDescription("WizardStaffBoyzz");
+
+        // Set max on the chart to 10 for now, should never go above 3 on demo
+        mChart.setMaxVisibleValueCount(10);
+
+        // Scaling can now only be done on the x and y-axis separately
+        mChart.setPinchZoom(false);
+
+        // Disable background
+        mChart.setDrawGridBackground(false);
+
+//        mTf = Typeface.createFromAsset(getAssets(), "OpenSans-Regular.ttf");
+
+        XAxis xAxis = mChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+//        xAxis.setTypeface(mTf);
+        xAxis.setDrawGridLines(false);
+
+        ValueFormatter custom = new MyValueFormatter();
+
+        YAxis leftAxis = mChart.getAxisLeft();
+//        leftAxis.setTypeface(mTf);
+        leftAxis.setLabelCount(8);
+        leftAxis.setValueFormatter(custom);
+
+        YAxis rightAxis = mChart.getAxisRight();
+        rightAxis.setDrawGridLines(false);
+//        rightAxis.setTypeface(mTf);
+        rightAxis.setLabelCount(8);
+        rightAxis.setValueFormatter(custom);
+
+//        Legend l = mChart.getLegend();
+//        l.setPosition(Legend.LegendPosition.BELOW_CHART_LEFT);
+//        l.setForm(Legend.LegendForm.SQUARE);
+//        l.setFormSize(9f);
+//        l.setTextSize(11f);
+//        l.setXEntrySpace(4f);
     }
 
 
@@ -35,5 +95,21 @@ public class ScoreboardActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onValueSelected(Entry e, int dataSetIndex, Highlight h) {
+        if (e == null)
+            return;
+
+        RectF bounds = mChart.getBarBounds((BarEntry) e);
+        PointF position = mChart.getPosition(e, YAxis.AxisDependency.LEFT);
+
+        Log.i("bounds", bounds.toString());
+        Log.i("position", position.toString());
+    }
+
+    public void onNothingSelected() {
+        // STUB
     }
 }
