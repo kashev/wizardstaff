ad3a65f587a9e475dcb54189f08a5035ec37244c
sulaimn2 <sulaimn2@illinois.edu>
2015-02-28 12:10:28 -0600

    Change from Pub Sub to Polling
    Polling from Android, remove music

--- spark/wizardstaff.cpp
+++ spark/wizardstaff.cpp
@@ -1,21 +1,3 @@
-/****** MUSIC STUFF ************/
-#define NOTE_BF4	1073
-#define NOTE_AF4	1205
-#define NOTE_F4		1433
-#define NOTE_EF5	804
-#define NOTE_D5		852
-#define PAUSE		50000
-#define MAX_NOTES		11
-
-int funkyTown[12] = {NOTE_BF4, NOTE_BF4, NOTE_AF4, NOTE_BF4, 
-					PAUSE, NOTE_F4, PAUSE, NOTE_F4,
-					NOTE_BF4, NOTE_EF5, NOTE_D5, NOTE_BF4};
-
-int16_t audioPin = D6;
-int16_t x,y = 0;
-/****** END MUSIC STUFF ************/
-
-
 // Define the pins we're going to call pinMode on
 int pwout = D0;  // You'll need to wire an LED to this one to see it blink.
 int led2 = D1; // This one is the built-in tiny one to the right of the USB jack
@@ -42,12 +24,20 @@ void setup() {
 //polls the status of the cup, return 1 if full, 0 if empty
 int checkfull(String args)
 {
-  if(!val)//if cup is empty, reset flag
+  if(!val && flag)//if cup is empty, reset flag
   {
       flag = 0;
+      return 0;
   }
-  return val;
-
+  else if (!val && !flag)//if cup is empty and flag has already been reset
+  {
+      return 2;
+  }
+  else // cup is full and user has been asked to take a drink
+  {
+    return 1;
+  }
+    
 }
 
 //asks the user to take a drink, returns -1 if the cup is empty, returns 1 if success
@@ -57,7 +47,7 @@ int takedrink(String args)
   if(!val)
   {
       return -1;
-  }
+  }   
   flag = 1;
   lastTime = millis();
   return 1;
@@ -76,27 +66,6 @@ void sendEvent()
 }
 
 
-void takeMeTo(int pin, int note){
-	for(x = 0; x < (DURATION/(note*2)); x++) {
-	    PIN_MAP[pin].gpio_peripheral->BSRR = PIN_MAP[pin].gpio_pin; // HIGH
-	    delayMicroseconds(note);
-	    PIN_MAP[pin].gpio_peripheral->BRR = PIN_MAP[pin].gpio_pin;  // LOW
-	    delayMicroseconds(note);
-	  }
-	  y++;
-	  if(y >= MAX_NOTES) y = 0;
-	  delay(250);
-}
-
-void outputmusic()
-{
-    int i = 0;
-    for (i = 0; i < 12; i++) {
-    takeMeTo(audioPin, funkyTown[i]);
-    }
-}
-
-
 // This routine gets called repeatedly, like once every 5-15 milliseconds.
 // Spark firmware interleaves background CPU activity associated with WiFi + Cloud activity with your code. 
 // Make sure none of your code delays or blocks for too long (like more than 5 seconds), or weird things can happen.
@@ -110,12 +79,10 @@ void loop() {
       digitalWrite(led, HIGH); //led is on.
       digitalWrite(buzzer, HIGH); //led is on.
   }
-  else if (!val && flag) //if user has to take a drink but the cup is empty, output is off. //SEND event
+  else if (!val && flag) //if user has to take a drink but the cup is empty, output is off.
   {
       digitalWrite(led, LOW);
       digitalWrite(buzzer, LOW);
-      sendEvent(); //Send event to server, "Event Name is empty!"
-      flag = 0;
       
   }
   else //if user does not have to take a drink and the cup is empty, output is off
